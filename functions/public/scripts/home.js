96d80fffddd2852a482b185617f9281cd2fe82b0
Mayank Shekhar <mayankshekhar03@gmail.com>
2017-08-16 13:07:12 +0530

    finalized the design and behaviour of the organizations page

--- functions/public/scripts/home.js
+++ functions/public/scripts/home.js
@@ -1,3 +1,14 @@
 $('#options').click(function(){
     $('#admin-user').toggle();
 });
+
+$('#orgs').click(function(){
+    var list = $('#list-items');
+    list.toggle();
+    $('.welcome').toggle();
+    var isVisible = list.is(':visible');
+    
+    if (isVisible) {
+        //add the getorganizations list calling function here
+    }
+});
