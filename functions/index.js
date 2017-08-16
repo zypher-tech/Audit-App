53ba77a49e70abab6c07e862a6c629791166dffa
nanborklabs <nandhu12195@gmail.com>
2017-08-16 18:34:08 +0530

    Added Path , js,css working

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions');
 const express=require('express');
+
 const firebase=require('firebase-admin');
+ const path = require('path')
+
 const cors = require('cors')({origin: true});
 
 const firebaseApplication=firebase.initializeApp(
@@ -19,6 +22,9 @@ app.engine('hbs',engines.handlebars);
 app.set('views','./views');
 app.set('view engine','hbs');
 app.use(cors);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', index);
 
